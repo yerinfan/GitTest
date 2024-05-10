f718744e32bb88cf6183cb391b2284fddcb20c42
Lee YeEun <78542145+yerinfan@users.noreply.github.com>
2024-05-10 15:01:10 +0900

    Update HelloWorld.java
    add message from github

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git World");
-		System.out.println("깃허브 연동~");
+		System.out.println("깃허브 연동~ really?");
 	}
 
 }
